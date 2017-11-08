b106ef906167e7dab9987cb0f005fab7e419ab31
jason <jason19659@163.com>
2017-11-08 14:27:33 +0800

    支持7.0

--- app/src/main/java/com/lanyus/blocksecureflag/MainActivity.java
+++ app/src/main/java/com/lanyus/blocksecureflag/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(tb);
-        SharedPreferences sharedPreferences = getSharedPreferences("checkedApp", MODE_WORLD_READABLE + MODE_WORLD_WRITEABLE);
+        SharedPreferences sharedPreferences = getSharedPreferences("checkedApp", MODE_PRIVATE );
         checkedApps = JSON.parseArray(sharedPreferences.getString("checkedApps", JSON.toJSONString(checkedApps)), CheckedApp.class);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean save() {
-        SharedPreferences sharedPreferences = getSharedPreferences("checkedApp", MODE_WORLD_READABLE + MODE_WORLD_WRITEABLE);
+        SharedPreferences sharedPreferences = getSharedPreferences("checkedApp", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("checkedApps", JSON.toJSONString(checkedApps));
         return editor.commit();
